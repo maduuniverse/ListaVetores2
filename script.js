7f7afd6e1abf315bd6bbe4b63fba12784846860e
Maria Eduarda <0001204539@senaimgaluno.com.br>
2026-06-03 08:57:13 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ for (let i = 0; i < 5; i++) {
     numeros[i] = Number(prompt("Digite um número:"));
 }
 
-for (let i = 4; i >= 0; i--) {
-    alert(numeros[i]);
+let procurar = Number(prompt("Digite o número a procurar:"));
+
+if (numeros.includes(procurar)) {
+    alert("Número encontrado!");
+} else {
+    alert("Número não encontrado!");
 }
