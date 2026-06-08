c7a50b15ca6f20df63ad2e0abc6564156a418944
Maria Eduarda <0001204539@senaimgaluno.com.br>
2026-06-08 07:49:31 -0300

    Create AT10.js

---
+++ AT10.js
@@ -0,0 +1,18 @@
+let vetorA = [1, 2, 3, 4, 5];
+let vetorB = [1, 2, 3, 4, 5];
+let vetorC = [1, 2, 3, 4, 5];
+
+for (let i = 0; i < 5; i++) {
+    vetorA[i] = Number(prompt("Digite um valor para o vetor A:"));
+}
+
+for (let i = 0; i < 5; i++) {
+    vetorB[i] = Number(prompt("Digite um valor para o vetor B:"));
+}
+
+for (let i = 0; i < 5; i++) {
+    vetorC.push(vetorA[i]);
+    vetorC.push(vetorB[i]);
+}
+
+alert(vetorC);
