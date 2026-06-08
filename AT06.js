b5123c93075c08aac556265e7a0345ca8049e2af
Maria Eduarda <0001204539@senaimgaluno.com.br>
2026-06-08 07:40:41 -0300

    Create AT06.js

---
+++ AT06.js
@@ -0,0 +1,13 @@
+let numeros = [];
+
+for (let i = 0; i < 5; i++) {
+    numeros[i] = parseInt(prompt("Digite um número:"));
+}
+
+for (let i = 0; i < 5; i++) {
+    if (numeros[i] < 0) {
+        numeros[i] = 0;
+    }
+}
+
+console.log(numeros);
